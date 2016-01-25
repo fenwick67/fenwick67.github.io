d93f6a1a6d28ac529b4c423384e6e438abd4c59a
fenwick67 <smileman66@gmail.com>
2016-01-24 20:38:06 -0600

    better duck model

--- duckgame/js/game.js
+++ duckgame/js/game.js
@@ -29,7 +29,6 @@ var isMobile = false; //initiate as false
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 
-  
   //HACK
   //HACK
 if (isMobile){
@@ -50,9 +49,9 @@ function init() {
   onWindowResize();
 
 	scene = new THREE.Scene();
-	scene.fog = new THREE.FogExp2( 0xffffff, 0.004 );
+	scene.fog = new THREE.FogExp2( 0xA4F7FF, 0.004 );
   
-  var skyMatl = new THREE.MeshLambertMaterial({side:THREE.BackSide,fog:false,color:0x00ccee});
+  var skyMatl = new THREE.MeshLambertMaterial({side:THREE.BackSide,fog:false,color:0x7992A9});
   var skyDome = new THREE.Mesh( new THREE.SphereGeometry( 300, 20, 10 ), skyMatl );
   scene.add(skyDome);
 	// Add Blender exported Collada model
@@ -60,7 +59,7 @@ function init() {
 
 	var loader = new THREE.JSONLoader();
 
-	loader.load('models/animated/monster/duck-rigged.json', function (geometry, materials) {
+	loader.load('models/animated/monster/duck-rigged-2anims.json', function (geometry, materials) {
 
 		var material = materials[0];
 		material.skinning = true;
@@ -82,7 +81,7 @@ function init() {
 		scene.add(mesh);
 
 		mixer = new THREE.AnimationMixer(mesh);
-		mixer.addAction(new THREE.AnimationAction(mesh.geometry.animations[1]));
+		mixer.addAction(new THREE.AnimationAction(mesh.geometry.animations[2]));
 
 		animate();
 
@@ -111,7 +110,7 @@ function init() {
 	// Lights
   
   var pointLight = new THREE.PointLight(0xffffff);
-  pointLight.intensity = 1;
+  pointLight.intensity = .8;
   pointLight.position.set(0,99,0);
   scene.add(pointLight);
 
@@ -136,7 +135,7 @@ function init() {
 	//scene.add(spotLightHelper);
   
   //ambient light
-	scene.add(new THREE.AmbientLight(0xcceeee));
+	scene.add(new THREE.AmbientLight(0x99eeee));
 
 	// Renderer
 
@@ -437,6 +436,7 @@ function playerCubeCollision(obj,width,height){
     return true;
 }
 
+//zoomed for loading screen
 function setCameraZoomed(){
   camera.position.set(  5.275489271607287,14.650010427656056,-13.235869197100351);
   camera.lookAt(duck.position);
