7d72f548ea1045b3172528975068ae352824e12c
fenwick67 <smileman67@gmail.com>
2017-04-11 18:25:38 -0500

    update publish script

--- publish.sh
+++ publish.sh
@@ -3,9 +3,11 @@
 # publish to github pages from the src branch
 branch=`git rev-parse --abbrev-ref HEAD`
 
-if [ branch == 'src' ]
+if [ ${branch} == 'src' ]
 then
+  echo "pushing..."
   git subtree push --prefix public origin master
 else
-  echo "you\'re on the wrong branch, dummy"
+  echo "you're on the wrong branch, dummy: "
+  echo "you're on: '${branch}', expected 'src'"
 fi
