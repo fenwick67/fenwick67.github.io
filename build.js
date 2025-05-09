c0308fa45e5e56d7027afb33a8bb2c25c2aa613a
fenwick67 <smileman66@gmail.com>
2025-05-09 15:26:17 -0400

    a

--- build.js
+++ build.js
@@ -1,17 +1,20 @@
 const fs = require("node:fs/promises")
 const yaml = require("yaml")
-const pug = require('pug')
+const pug = require('pug');
+const path = require("node:path");
 
 async function build(opts){
     await fs.mkdir("dist",{recursive: true});
-    (await fs.readdir("src")).filter(s=>s.indexOf('yml') == s.length - 3)
-    .forEach(async (ymlFilename)=>{
-            const shortname = ymlFilename.replace(/\..+$/gi,"");
-            const data = yaml.parse(await fs.readFile(`./src/${ymlFilename}`,'utf-8'));
-            const template = pug.compile(await fs.readFile(`./src/${shortname}.pug`),opts);
-            fs.writeFile(`dist/${shortname}.html`, template(data));
-        }
-    );
+    (await fs.readdir("src")).filter(s=>s.indexOf('yml') == s.length - 3).forEach(async (ymlFilename)=>{
+        const shortname = ymlFilename.replace(/\..+$/gi,"");
+        const data = yaml.parse(await fs.readFile(`./src/${ymlFilename}`,'utf-8'));
+        const template = pug.compile(await fs.readFile(`./src/${shortname}.pug`),opts);
+        fs.writeFile(`dist/${shortname}.html`, template(data));
+    });
+
+    (await fs.readdir("static")).forEach(async (s)=>{
+        fs.cp(path.join("static",s), path.join("dist",s),{recursive:true});
+    });
 }
 
 module.exports= {build}
